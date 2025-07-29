a240fbdec22b23a014c3138d01e5cfa832d18408
acodeai <222967865+acodeai@users.noreply.github.com>
2025-07-29 20:09:14 +0530

    file add mapping.lua

---
+++ mappings.lua
@@ -0,0 +1,11 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({"n"}, "<C-z>", "<cmd>undo<CR>")
+
